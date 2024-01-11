b561a149e911ddf72444d2ef866ff236c6411d5f
Mauricio Goiz Sarmiento <mauricio>
2024-01-10 19:33:03 -0600

    Protip: Asignaciones con operaciones

--- 01-fundamentos/assets/js/logica-booleana.js
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -36,4 +36,24 @@ console.log( false || false);
 console.log( regresaTrue() || regresaFalse() );
 console.log( '4 condiciones', true || true || true || false); // true
 
+
+
 console.warn('Asignaciones');
+
+const soyUndefined = undefined;
+const sonNull = null;
+const soyFalso = false;
+
+const a1 = true && 'Hola mundo' && 150; 
+const a2 = 'Hola' && 'Mundo' && soyFalso && true;
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyUndefined || sonNull || 'Ya no soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo' || true;
+
+console.log({ a1, a2, a3, a4, a5 });
+
+if ( regresaFalse() && regresaTrue() && ( true || false || true )) {
+    console.log('Hacer algo');
+} else {
+    console.log('Hacer otra cosa');
+}
