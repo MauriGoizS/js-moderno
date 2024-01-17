5ca545ef495a3c30152094876be003ac7a5e8fd8
Mauricio Goiz Sarmiento <mauricio>
2024-01-16 21:07:11 -0600

    Pedir carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -7,6 +7,8 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+
+/* Esta función crea un nuevo deck */
 const crearDeck = () => {
     for( let i = 2; i <= 10; i++) {
         //deck.push( i + 'C');
@@ -20,10 +22,32 @@ const crearDeck = () => {
             deck.push( especial + tipo);
         }
     }
-    console.log( deck );
+
+    /* console.log( deck ); */
     deck = _.shuffle( deck );
     console.log( deck );
     return deck;
 }
 
 crearDeck();
+
+
+/* Esta función me permite tomar una carta */
+const pedirCarta = () => {
+
+    if (deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop();
+
+    console.log( deck );
+    console.log( carta );
+    return carta;
+}
+
+/* for (let i = 0; i <=100; i++) {
+    pedirCarta();
+} */
+
+pedirCarta();
