302ae4e33ad7d1cfac3c5583dbe823adb7232bcd
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-07-02 22:41:41 -0600

    Mejorar la documentación JSDoc comments

--- 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
+++ 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
@@ -1,9 +1,17 @@
 import _ from "underscore";
 
-export const miNombre = 'Mauricio';
+// export const miNombre = 'Mauricio';
 
-// Esta función crea un nuevo deck
+/**
+ * Esta función crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta Ejemplo: ['C','D','H','S'];
+ * @param {Array<String>} tiposEspeciales Ejemplo: ['A','J','Q','K'];
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    if ( !tiposDeCarta || !tiposDeCarta.length === 0 ) throw new Error('tiposDeCarta es obligatorio como un arreglo de String');
+    if ( !tiposEspeciales || !tiposEspeciales.length === 0 ) throw new Error('tiposEspeciales es obligatorio como un arreglo de String');
 
     let deck = [];
 
