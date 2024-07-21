97a7523815f5f7dd1962fc4ee513aceecd97e2a5
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-07-20 22:47:25 -0600

    Métodos que ocuparemos en el Store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -23,6 +23,49 @@ const initStore = () => {
     console.log('InitStore ');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+    
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todId 
+ */
+const toggleTodo = ( todId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = ( todId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
     initStore,
+    loadStore,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
 }
