cdbb91a397b2e191f163ecd4358197066b50082a
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-22 00:25:52 -0600

    Variables de entorno

--- 06-advanced/main.js
+++ 06-advanced/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { environmentsComponent } from './src/concepts/01-environments'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -17,4 +18,6 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-console.log('object');
+const element = document.querySelector('.card');
+
+environmentsComponent( element );
