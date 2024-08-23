dd7ae4808a9c5cf59bdbd8b5122030f2a3ff8ab2
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-23 00:03:14 -0600

    Errores en Async-Await

--- 06-advanced/src/concepts/06-async-await.js
+++ 06-advanced/src/concepts/06-async-await.js
@@ -6,13 +6,25 @@ import { heroes } from "../data/heroes";
  */
 export const asyncAwaitComponent = async( element ) => {
 
-    const id1 = '5d86371fd55e2e2a30fe1ccb';
+    const id1 = '5d86371fd55e2e2a30fe1ccbz';
     const id2 = '5d86371f97c29d020f1e1f6d';
 
-    const hero1 = await findHero( id1 );
-    const hero2 = await findHero( id2 );
+    
 
-    element.innerHTML = ` ${ hero1.name } / ${ hero2.name }`;
+    try {
+
+        const hero1 = await findHero( id1 );
+        const hero2 = await findHero( id2 );
+
+        element.innerHTML = ` ${ hero1.name } / ${ hero2.name }`;
+        
+    } catch ( error ) {
+
+        element.innerHTML = error;
+
+    }
+
+    
 }
 
 const findHero = async( id ) => {
