313d7bce0f6f46c846acc19c7e84f4c8035ecf90
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-22 23:05:36 -0600

    Async

--- 06-advanced/main.js
+++ 06-advanced/main.js
@@ -5,6 +5,7 @@ import { environmentsComponent } from './src/concepts/01-environments'
 import { callbacksComponent } from './src/concepts/02-callbacks'
 import { promisesComponent } from './src/concepts/03-promises'
 import { promiseRaceComponent } from './src/concepts/04-promise-race'
+import { asyncComponent } from './src/concepts/async'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -26,4 +27,5 @@ const element = document.querySelector('.card');
 // environmentsComponent( element );
 // callbacksComponent( element );
 // promisesComponent( element );
-promiseRaceComponent( element );
+// promiseRaceComponent( element );
+asyncComponent( element );
