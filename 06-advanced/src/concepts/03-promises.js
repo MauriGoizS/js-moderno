9de3de6bc1db1c60dc30e02839b8e95c571c9961
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-22 22:04:07 -0600

    Promise - All

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -27,20 +27,40 @@ export const promisesComponent = ( element ) => {
     const id1 = '5d86371f2343e37870b91ef1'
     const id2 = '5d86371fd55e2e2a30fe1cc3'
 
+    Promise.all([
+        findHero(id1),
+        findHero(id2),
+    ])
+    .then( ([hero1, hero2] ) => renderTwoHeroes( hero1, hero2 ) )
+    .catch( renderError );
 
-    findHero( id1 )
-        .then( ( hero1 ) => {
+
+    //! Forma 2 
+    // let hero1;
+    // findHero( id1 )
+    //     .then( hero => {
+    //         hero1 = hero;
+    //         return findHero( id2 );
+    //     }).then( hero2 => {
+    //         renderTwoHeroes( hero1, hero2 );
+    //     })
+    //     .catch( renderError );
+
+
+    //! Forma 1
+    // findHero( id1 )
+    //     .then( ( hero1 ) => {
             
-            findHero( id2 )
-                .then( hero2 => {
-                    renderTwoHeroes( hero1, hero2 );
-                })
+    //         findHero( id2 )
+    //             .then( hero2 => {
+    //                 renderTwoHeroes( hero1, hero2 );
+    //             })
 
-                .catch( renderError );
+    //             .catch( renderError );
 
-        })
+    //     })
 
-        .catch( renderError );
+    //     .catch( renderError );
 }
 
 
