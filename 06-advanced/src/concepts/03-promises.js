745d97851afea2c423e3903f5f2b52ebcf9c6bc2
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-22 21:39:49 -0600

    Promise Hell

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -10,6 +10,13 @@ export const promisesComponent = ( element ) => {
         element.innerHTML = hero.name;
     }
 
+    const renderTwoHeroes = ( hero1, hero2) => {
+        element.innerHTML = `
+            <h3> Hero 1: ${hero1.name} </h3>
+            <h3> Hero 2: ${hero2.name} </h3>
+        `;
+    }
+
     const renderError = ( error ) => {
         element.innerHTML = `
             <h1> Error: </h1>
@@ -20,9 +27,20 @@ export const promisesComponent = ( element ) => {
     const id1 = '5d86371f2343e37870b91ef1'
     const id2 = '5d86371fd55e2e2a30fe1cc3'
 
+
     findHero( id1 )
-        .then( renderHero )
-        .catch( error => renderError( error ) );
+        .then( ( hero1 ) => {
+            
+            findHero( id2 )
+                .then( hero2 => {
+                    renderTwoHeroes( hero1, hero2 );
+                })
+
+                .catch( renderError );
+
+        })
+
+        .catch( renderError );
 }
 
 
