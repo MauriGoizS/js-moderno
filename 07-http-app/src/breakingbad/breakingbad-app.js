b2d42471edc47f4684706b48a845146289a2cda8
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-23 22:46:29 -0600

    Mostrar quote en pantalla

--- 07-http-app/src/breakingbad/breakingbad-app.js
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -1,11 +1,41 @@
 
 /**
  * 
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async() => {
+
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+
+    console.log(data[0]);
+    return data[0];
+}
+
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = ( element ) => {
+export const BreakingbadApp = async( element ) => {
 
-    document.querySelector('#app-title').innerHTML = 'Breaking Bad app';
+    document.querySelector('#app-title').innerHTML = 'BreakingBad app';
     element.innerHTML = 'Loading...';
+
+    // await fetchQuote();
+
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = ( data ) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren( quoteLabel, authorLabel, nextQuoteButton );
+    }
+
+    fetchQuote()
+        .then( (data) => renderQuote(data) );
 
 }
