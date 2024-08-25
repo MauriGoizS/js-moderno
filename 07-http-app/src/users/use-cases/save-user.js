a676dd5543392780786894b4d08c5327cd75991d
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-24 23:46:30 -0600

    Caso de uso - Crear usuario

---
+++ 07-http-app/src/users/use-cases/save-user.js
@@ -0,0 +1,36 @@
+import { User } from '../models/user';
+
+export const saveUser = async( userLike ) => {
+
+    const user = new User( userLike );
+
+    // TODO: aquí falta un mapper
+
+    if( user.id ) {
+        throw 'No implementada la actualización';
+    }
+
+    const updatedUser = await createUser( user );
+    return updatedUser;
+}
+
+/**
+ * 
+ * @param {Like<User>} user 
+ */
+const createUser = async( user ) => {
+
+    const url = `${ import.meta.env.VITE_BASE_URL }/users`;
+    const res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    });
+
+    const newUser = await res.json();
+    console.log({ newUser });
+    return newUser;
+
+}
