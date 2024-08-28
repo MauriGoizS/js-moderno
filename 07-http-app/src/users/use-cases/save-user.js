89fd6ee06769148d269e9c799d54a3eebd1fdaff
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-27 21:36:32 -0600

    Mapper - userModelToLocalhost

--- 07-http-app/src/users/use-cases/save-user.js
+++ 07-http-app/src/users/use-cases/save-user.js
@@ -1,16 +1,20 @@
+import { userModelToLocalhost } from '../mappers/user-to-localhost.mapper';
 import { User } from '../models/user';
 
 export const saveUser = async( userLike ) => {
 
     const user = new User( userLike );
 
-    // TODO: aquí falta un mapper
+    if ( !user.firstName || !user.lastName ) 
+        throw "First & last name name required";
+
+    const userToSave = userModelToLocalhost( user );
 
     if( user.id ) {
         throw 'No implementada la actualización';
     }
 
-    const updatedUser = await createUser( user );
+    const updatedUser = await createUser( userToSave );
     return updatedUser;
 }
 
