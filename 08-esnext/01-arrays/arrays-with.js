73425d02ac53170013b53c33defaeb4d5ff8f8dd
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-08-29 22:48:56 -0600

    structuredClone

--- 08-esnext/01-arrays/arrays-with.js
+++ 08-esnext/01-arrays/arrays-with.js
@@ -1,2 +1,26 @@
 
-console.log('Hola mundo');
+const superHeroes = [
+    {
+        id: 1,
+        name: 'Batman'
+    },
+    {
+        id: 2,
+        name: 'Superman'
+    },
+    {
+        id: 3,
+        name: 'Flash'
+    },
+    {
+        id: 4,
+        name: 'Aquaman'
+    },
+];
+
+const superHeroesCopy = structuredClone( superHeroes );
+
+superHeroesCopy[0].name = 'Green Lantern';
+
+console.table( superHeroes );
+console.table( superHeroesCopy );
